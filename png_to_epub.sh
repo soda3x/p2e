2b9cf1c5b3990e53bf1cc5cdf93f4a9646499d54
Bradley Newman <bnewman227@gmail.com>
2025-11-04 21:18:06 +1030

    Updates to make it better adhere to the EPUB 3 standard, actually works in Komga properly now

--- png_to_epub.sh
+++ png_to_epub.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+shopt -s extglob
 
 TITLE="${1:-Unknown}"
 
 PREFIX_TO_TRIM="./output/"
 
-cp -r template/* output/
+cp -r template/!(page.xhtml) output/
 
 pngs=()
 pages=()
-html_elems=()
+spine_elems=()
 
+# Get just the filenames of the images in ./output
 for filepath in ./output/images/*; do
   if [ -f "$filepath" ]; then
     page="${filepath#$PREFIX_TO_TRIM}"
@@ -19,38 +21,55 @@ done
 
 echo "Processing ${#pngs[@]} pages..."
 
+# Create the cover image element for the content.opf
+cover="<item href=\"${pngs[0]}\" id=\"cover_image_id\" media-type=\"image/png\" properties=\"cover-image\"/>"
+
+# Create the things required for the content.opf file
 for index in "${!pngs[@]}"; do
   value="${pngs[index]}"
-  page="<item href=\"$value\" id=\"id$((index + 1))\" media-type=\"image/png\"/>"
+  pageNum="$((index + 1))"
+
+  # Page reference elements
+  page="<item href=\"page$pageNum.xhtml\" id=\"page$pageNum\" media-type=\"application/xhtml+xml\" properties=\"svg rendition:layout fixed\"/>"
+  # Need to also add the image itself to the manifest
+  img="<item href=\"$value\" id=\"img$pageNum\" media-type=\"image/png\"/>"
   pages+=("$page"$'\n')
+  pages+=("$img"$'\n')
+
+  # Spine elements
+  spine="<itemref idref=\"page$pageNum\"/>"
+  spine_elems+=("$spine"$'\n')
+
+  # Create an xhtml file for the page
+  PAGE_FILE="./output/page$pageNum.xhtml"
+  cp template/page.xhtml $PAGE_FILE
+  perl -pi -e "s|IMAGE_REF|${value}|g" $PAGE_FILE
+  perl -pi -e "s|PAGE_NUM|Page ${pageNum}|g" $PAGE_FILE
 done
 
-for index in "${!pngs[@]}"; do
-  value="${pngs[index]}"
-  elem="<p class=\"paragraph\"><a id=\"id$((index + 1))\"></a><img src=\"$value\" class=\"image\"/></p>"
-  html_elems+=("$elem"$'\n')
-done
-
-
+# Keep the old IFS to restore after
 OLD_IFS="$IFS"
+
+# Create new IFS
 IFS=''
 
 # Expand the array into a single string
 pages_str="${pages[*]}"
-elems_str="${html_elems[*]}"
+elems_str="${spine_elems[*]}"
 
 # Restore the original IFS
 IFS="$OLD_IFS"
 
+# Generate a UUID to identify the book
 uuid=$(uuidgen)
 
-# Now do some token replacing and whatnot
+# Now populate the content.opf file
+CONTENT_OPF_FILE=./output/content.opf
 
-perl -pi -e "s|CONTENT|${pages_str//|SPLIT|/}|g" ./output/content.opf
-perl -pi -e "s|AUTHOR|Unknown|g" ./output/content.opf
-perl -pi -e "s|CONTRIB|Unknown|g" ./output/content.opf
-perl -pi -e "s|TITLE|${TITLE}|g" ./output/content.opf
-perl -pi -e "s|UUID|${uuid}|g" ./output/content.opf
-perl -pi -e "s|CONTENT|${elems_str//|SPLIT|/}|g" ./output/index.html
-perl -pi -e "s|TITLE|${TITLE}|g" ./output/toc.ncx
-perl -pi -e "s|UUID|${uuid}|g" ./output/toc.ncx
+perl -pi -e "s|COVER|${cover}|g" $CONTENT_OPF_FILE
+perl -pi -e "s|CONTENT|${pages_str//|SPLIT|/}|g" $CONTENT_OPF_FILE
+perl -pi -e "s|SPINE|${elems_str//|SPLIT|/}|g" $CONTENT_OPF_FILE
+perl -pi -e "s|AUTHOR|Unknown|g" $CONTENT_OPF_FILE
+perl -pi -e "s|CONTRIB|Unknown|g" $CONTENT_OPF_FILE
+perl -pi -e "s|TITLE|${TITLE}|g" $CONTENT_OPF_FILE
+perl -pi -e "s|UUID|${uuid}|g" $CONTENT_OPF_FILE
